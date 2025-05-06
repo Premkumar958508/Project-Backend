32f38088c00cf88357e6ad33ccdff10d0351e1d3
YahiyaKhan12345 <123312902+YahiyaKhan12345@users.noreply.github.com>
2025-05-06 09:53:53 +0530

    Initial commit

---
+++ cost-estimation-service/src/main/java/com/example/CostEstimation/Feign/InputServiceClient.java
@@ -0,0 +1,14 @@
+package com.example.CostEstimation.Feign;
+
+import com.example.CostEstimation.Dto.InputDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-input-service", url = "http://localhost:8080") // Update with actual service name if using Eureka
+public interface InputServiceClient {
+
+    @GetMapping("/api/inputs/getid/{inputId}")
+    InputDTO getInputDetails(@PathVariable("inputId") Long inputId);
+}
+
